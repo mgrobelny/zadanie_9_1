fe80092dddf8b0f7b19a29d426688f14bad80a31
mgrobelny <michalmaciejg@gmail.com>
2017-09-02 18:33:26 +0200

    init

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// scripts.js
+
+function getTriangleArea(a, h) {
+
+if (a>0 && h>0) {
+	return 'Pole trójkąta to: ' +a*h/2;
+}
+else {
+	return 'Dane nieprawidłowe';
+}
+
+}
+console.log( getTriangleArea(11,6) );
+
+var triangle1Area = getTriangleArea(10, 15);
+console.log(triangle1Area);
+
+var triangle2Area = getTriangleArea(11, 18);
+console.log(triangle2Area);
+
+var triangle3Area = getTriangleArea(13, 18);
+console.log(triangle3Area)
